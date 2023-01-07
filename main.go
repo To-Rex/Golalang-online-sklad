392f01da8819a5ec51ef3763e5df2d123ce32237
Yorvoration <dev.dilshodjon@gmail.com>
2023-01-07 18:33:20 +0500

    desctop app

--- main.go
+++ main.go
@@ -141,12 +141,13 @@ func main() {
 	router.OPTIONS("/deleteUser", HandleFoo)
 	router.Run()
 }
-
 func HandleFoo(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.AbortWithStatus(200)
 	c.Next()
 }
 
