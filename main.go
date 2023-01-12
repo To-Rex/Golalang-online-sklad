aec7ed2086ab98834a0d4f1dedc87512bebd2825
Yorvoration <dev.dilshodjon@gmail.com>
2023-01-12 06:40:47 +0500

    web app

--- main.go
+++ main.go
@@ -135,7 +135,7 @@ func main() {
 	// config.AllowCredentials = true
 	// router.Use(cors.New(config))
 	router.Use(func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "file:///Users/yorvoration/Desktop/Eduvi/index.html")
+		c.Header("Access-Control-Allow-Origin", "file:///Users/yorvoration/Desktop/Eduvi/registration.html")
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
